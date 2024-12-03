e74da71b3683be2bacf8ddf4f1a06d2623a7d2c0
JonArbell <138678861+JonArbell@users.noreply.github.com>
2024-12-03 20:44:43 +0800

    initial responsive of project section and add initial hobbies bg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from "./main/hero-section/hero-section.component";
 import { AboutMeComponent } from './main/about-me/about-me.component';
 import { ProjectsComponent } from './main/projects/projects.component';
+import { HobbiesComponent } from './main/hobbies/hobbies.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent, AboutMeComponent,ProjectsComponent],
+  imports: [RouterOutlet, HeroSectionComponent, AboutMeComponent,ProjectsComponent,HobbiesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
