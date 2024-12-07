5047eeca7bf478d94175b220974eead4f417e0ad
JonArbell <138678861+JonArbell@users.noreply.github.com>
2024-12-07 18:41:00 +0800

    minimize the subtitle of skills and projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { ProjectsComponent } from './main/projects/projects.component';
 import { HobbiesComponent } from './main/hobbies/hobbies.component';
 import { FooterComponent } from './main/footer/footer.component';
 import { SkillsComponent } from './main/skills/skills.component';
+import { ContactMeComponent } from './main/contact-me/contact-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent, AboutMeComponent,ProjectsComponent,SkillsComponent,HobbiesComponent,FooterComponent],
+  imports: [RouterOutlet, HeroSectionComponent, AboutMeComponent,ProjectsComponent,SkillsComponent,HobbiesComponent,FooterComponent, ContactMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/main/contact-me/contact-me.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-me',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-me.component.html'
+})
+export class ContactMeComponent {
+
+}
