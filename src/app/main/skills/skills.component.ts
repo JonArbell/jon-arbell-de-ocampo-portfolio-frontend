1e4651dd6d2e85c47c51e0a0796d137ed66e37cc
JonArbell <138678861+JonArbell@users.noreply.github.com>
2024-12-07 19:33:11 +0800

    minimize the code of skills section using Input Binding

--- src/app/main/skills/skills.component.ts
+++ src/app/main/skills/skills.component.ts
@@ -8,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+  stacks : TechStacks[] = [
+    new TechStacks('Java','assets/img/java.png'),
+    new TechStacks('SpringBoot','assets/img/spring.png'),
+    new TechStacks('PostgreSQL','assets/img/postgresql.png'),
+    new TechStacks('JavaScript','assets/img/js.png'),
+    new TechStacks('TypeScript','assets/img/ts.png'),
+    new TechStacks('Angular','assets/img/angular.png'),
+    new TechStacks('CSS 3','assets/img/css-3.png'),
+    new TechStacks('Tailwind CSS','assets/img/tailwind.png'),
+    new TechStacks('GitHub','assets/img/github.png')
+  ];
+
+}
+
+class TechStacks{
+  constructor(private techName : string,private imagePath : string) {}
+
+  getTechName() : string{
+    return this.techName;
+  }
+
+  getImagePath() : string{
+    return this.imagePath;
+  }
 }
