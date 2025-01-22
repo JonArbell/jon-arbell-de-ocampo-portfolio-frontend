9cb6d8037e49caf9a12f7e56857a4cb2059a1d97
JonArbell <138678861+JonArbell@users.noreply.github.com>
2025-01-22 20:58:42 +0800

    remove the version of css

--- src/app/main/skills/skills.component.ts
+++ src/app/main/skills/skills.component.ts
@@ -10,12 +10,12 @@ export class SkillsComponent {
 
   stacks : TechStacks[] = [
     new TechStacks('Java','assets/img/java.png'),
-    new TechStacks('SpringBoot','assets/img/spring.png'),
+    new TechStacks('SpringBoot ','assets/img/spring.png'),
     new TechStacks('PostgreSQL','assets/img/postgresql.png'),
     new TechStacks('JavaScript','assets/img/js.png'),
     new TechStacks('TypeScript','assets/img/ts.png'),
     new TechStacks('Angular','assets/img/angular.png'),
-    new TechStacks('CSS 3','assets/img/css-3.png'),
+    new TechStacks('CSS','assets/img/css-3.png'),
     new TechStacks('Tailwind CSS','assets/img/tailwind.png'),
     new TechStacks('GitHub','assets/img/github.png')
   ];
