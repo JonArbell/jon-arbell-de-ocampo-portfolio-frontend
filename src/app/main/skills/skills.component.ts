beba0a0d7f085bcff4c48e816d13ef3466be312c
JonArbell <138678861+JonArbell@users.noreply.github.com>
2025-01-26 20:45:09 +0800

    add versions

--- src/app/main/skills/skills.component.ts
+++ src/app/main/skills/skills.component.ts
@@ -9,12 +9,12 @@ import { Component } from '@angular/core';
 export class SkillsComponent {
 
   stacks : TechStacks[] = [
-    new TechStacks('Java','assets/img/java.png'),
-    new TechStacks('SpringBoot ','assets/img/spring.png'),
+    new TechStacks('Java 23','assets/img/java.png'),
+    new TechStacks('Spring Boot 3','assets/img/spring.png'),
     new TechStacks('PostgreSQL','assets/img/postgresql.png'),
     new TechStacks('JavaScript','assets/img/js.png'),
     new TechStacks('TypeScript','assets/img/ts.png'),
-    new TechStacks('Angular','assets/img/angular.png'),
+    new TechStacks('Angular 19','assets/img/angular.png'),
     new TechStacks('CSS','assets/img/css-3.png'),
     new TechStacks('Tailwind CSS','assets/img/tailwind.png'),
     new TechStacks('GitHub','assets/img/github.png')
