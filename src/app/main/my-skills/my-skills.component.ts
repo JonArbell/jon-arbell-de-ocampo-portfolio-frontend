e2f1fbde444d60b57849781e2ca269a58607443b
JonArbell <138678861+JonArbell@users.noreply.github.com>
2025-03-06 12:29:09 +0800

    Add initial design for skills component

--- src/app/main/my-skills/my-skills.component.ts
+++ src/app/main/my-skills/my-skills.component.ts
@@ -7,15 +7,15 @@ import { Component } from '@angular/core';
 })
 export class MySkillsComponent {
   stacks : TechStacks[] = [
-    new TechStacks('Java 23','assets/img/java.png'),
-    new TechStacks('Spring Boot 3','assets/img/spring.png'),
-    new TechStacks('PostgreSQL','assets/img/postgresql.png'),
-    new TechStacks('JavaScript','assets/img/js.png'),
-    new TechStacks('TypeScript','assets/img/ts.png'),
-    new TechStacks('Angular 19','assets/img/angular.png'),
-    new TechStacks('CSS','assets/img/css-3.png'),
-    new TechStacks('Tailwind CSS','assets/img/tailwind.png'),
-    new TechStacks('GitHub','assets/img/github.png')
+    new TechStacks('Java 23','/img/java.png'),
+    new TechStacks('Spring Boot 3','/img/spring.png'),
+    new TechStacks('PostgreSQL','/img/postgresql.png'),
+    new TechStacks('JavaScript','/img/js.png'),
+    new TechStacks('TypeScript','/img/ts.png'),
+    new TechStacks('Angular 19','/img/angular.png'),
+    new TechStacks('CSS','/img/css-3.png'),
+    new TechStacks('Tailwind CSS','/img/tailwind.png'),
+    new TechStacks('GitHub','/img/github.png')
   ];
 }
 
