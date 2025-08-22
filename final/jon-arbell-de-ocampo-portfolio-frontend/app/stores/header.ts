f6c3c5c6990c907d6e488234c4498a9d9380ec27
Jon Arbell De Ocampo <deocampo.jonarbell@primex.ventures>
2025-08-23 00:21:41 +0800

    test

--- final/jon-arbell-de-ocampo-portfolio-frontend/app/stores/header.ts
+++
@@ -1,7 +0,0 @@
-import { defineStore } from "pinia";
-
-export const useMyHeaderStore = defineStore("myHeaderStore", () => {
-  const background = ref<string>("bg-transparent");
-
-  return { background };
-});
--- final/jon-arbell-de-ocampo-portfolio-frontend/app/stores/side-bar.ts
+++
@@ -1,9 +0,0 @@
-import { defineStore } from "pinia";
-
-export const useMySideBarStore = defineStore("mySideBarStore", () => {
-  const isSideBarOpen = ref<boolean>(false);
-
-  const isFirstLoad = ref<boolean>(true);
-
-  return { isSideBarOpen, isFirstLoad };
-});
